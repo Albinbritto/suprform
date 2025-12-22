d70253c3fafefcace1a2ec5c2c780ba817287800
Albinbritto <albinbritto1234@gmail.com>
2025-12-22 20:44:16 +0530

    test commit

--- src/util.ts
+++ src/util.ts
@@ -7,6 +7,7 @@ export function conditionEvaluator<TFieldValues extends FieldValues>(
   condition: Visibility<TFieldValues>
 ): boolean {
   const { conditions, operator } = condition;
+
   const valueByName: Record<string, any> = namesToWatch.reduce((acc, name, idx) => {
     acc[name as string] = watchedValues[idx];
     return acc;
